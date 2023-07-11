ce85923ed37a880f09c04453417266c4b09c0829
beamstar-system <124396260+beamstar-system@users.noreply.github.com>
2023-07-11 05:12:03 -0400

    update

--- module.js
+++ module.js
@@ -1,71 +1,41 @@
-const fs = require('fs').promises;
-
 let somiibo;
 let counter = 0;
-
-let proxies = new Set();
-let userAgents = new Set();
-
-fs.readFile('/proxy.txt', 'utf-8')
-  .then(data => proxies = new Set(data.split('\n')))
-  .catch(err => console.error(err));
-
-fs.readFile('/ua.txt', 'utf-8')
-  .then(data => userAgents = new Set(data.split('\n')))
-  .catch(err => console.error(err));
-
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
 
 async function main(mod) {
   somiibo = mod;
 
-  for (counter = 0; counter < 100000; counter++) {
-    let proxy = getRandomElement(Array.from(proxies));
-    let userAgent = getRandomElement(Array.from(userAgents));
+  counter++;
 
-    try {
-      await somiibo.worker(async function (counter) {
-        try {
-          if (!window.location.href.includes('/search/')) {
-            document.querySelector('input[name="q"').value = `site:https://hweeb.medium.com/learn-what-an-nft-is-exploring-hweeb-eths-take-5260f8f0b42f`;
-            document.querySelectorAll('input[type="submit"]')[2].click();
-          } else {
-            let scrollTimes = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
-            for(let i = 0; i < scrollTimes; i++) {
-              await somiibo.browser().scroll({x: 0, y: document.body.scrollHeight}, {offsetX: 0, offsetY: -50});
-            }
-
-            let waitTime = Math.random() * (20 - 5) + 5; // Random number between 5 and 20
-            await new Promise(resolve => setTimeout(resolve, waitTime * 60 * 1000));
-          }
-        } catch (e) {
-          somiibo.log('Error in worker function', e);
-        }
-      }, {
-        arguments: [counter],
-        url: 'https://hweeb.medium.com/learn-what-an-nft-is-exploring-hweeb-eths-take-5260f8f0b42f',
-        width: 600,
-        height: 800,
-        referrer: 'https://hweeb.medium.com/learn-what-an-nft-is-exploring-hweeb-eths-take-5260f8f0b42f',
-        timeout: 60000,
-        debug: false,
-        proxy: proxy,
-        userAgent: userAgent,
-      })
-      .then((worker) => {
-        somiibo.log(`Set up worker ${counter}`, worker);
-      })
-      .catch((e) => {
-        somiibo.log(`Failed to set up worker ${counter}`, e);
-      });
-    } catch (e) {
-      somiibo.log(`Failed to set up worker ${counter}`, e);
+  await somiibo.worker(function (counter) {
+    if (!window.location.href.includes('/search/')) {
+      document.querySelector('input[name="q"').value = `https://opensea.io/collection/hweebs`;
+      document.querySelectorAll('input[type="submit"]')[2].click();
     }
+  }, {
+    arguments: [counter],
+    url: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/53072707301300110528791128517643333770372762089746594648374035080322427750048',
+    // proxy: '',
+    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    width: 600,
+    height: 800,
+    referrer: 'https://google.com',
+    timeout: 60000,
+    debug: false,
+  })
+  .then((worker) => {
+    somiibo.log('Set up worker', worker);
+  })
+  .catch((e) => {
+    somiibo.log('Failed to set up worker', e);
+  });
+
+  // After we've made 3 workers we can stop the module
+  if (counter >= 100000) {
+    return somiibo.stop();
   }
 
-  return somiibo.stop();
+  // Run the main loop
+  return somiibo.loop(main);
 }
 
 module.exports = main;
